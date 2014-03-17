45e7a9f38dc2e357c7b1c393a270665f61047184
unknown <c010595@LATL-68Q3PX1.na.global.prv>
2014-03-17 09:56:51 -0400

    Push of ANN

--- MarioLevelComp2011_cs4731/MarioLevelComp2011_cs4731/src/dk/itu/mario/ann/NetworkNode.java
+++ MarioLevelComp2011_cs4731/MarioLevelComp2011_cs4731/src/dk/itu/mario/ann/NetworkNode.java
@@ -7,43 +7,43 @@ import java.util.Random;
 
 public class NetworkNode implements Serializable {
 
-	private static final long serialVersionUID = 4111158235251688552L;
-	
-	private double nodeValue;
-	
-	private List<Double> outputWeights;
-	
-	private Random randGen;
+    private static final long serialVersionUID = 4111158235251688552L;
 
-	public NetworkNode(int nodeValue, int numberOut) {
-		this.nodeValue = nodeValue;
-		outputWeights = new ArrayList<Double>();
-		randGen = new Random();
-		for (int i = 0; i < numberOut; i++) {
-			outputWeights.add((randGen.nextDouble() - 0.5) / 10.00);
-		}
-		return;
-	}
+    private double nodeValue;
 
-	public void setNodeValue(double nodeValue) {
-		this.nodeValue = nodeValue;
-		return;
-	}
+    private List<Double> outputWeights;
 
-	public void setOutputWeightAt(int index, double weight) {
-		outputWeights.set(index, weight);
-		return;
-	}
+    private Random randGen;
 
-	public void setOutPutWeight(List<Double> outputWeights) {
-		this.outputWeights = outputWeights;
-	}
+    public NetworkNode(int nodeValue, int numberOut) {
+        this.nodeValue = nodeValue;
+        outputWeights = new ArrayList<Double>();
+        randGen = new Random();
+        for (int i = 0; i < numberOut; i++) {
+            outputWeights.add((randGen.nextDouble() - 0.5) / 10.00);
+        }
+        return;
+    }
 
-	public double getNodeValue() {
-		return nodeValue;
-	}
+    public void setNodeValue(double nodeValue) {
+        this.nodeValue = nodeValue;
+        return;
+    }
 
-	public double getOutputWeightAt(int index) {
-		return outputWeights.get(index);
-	}
+    public void setOutputWeightAt(int index, double weight) {
+        outputWeights.set(index, weight);
+        return;
+    }
+
+    public void setOutPutWeight(List<Double> outputWeights) {
+        this.outputWeights = outputWeights;
+    }
+
+    public double getNodeValue() {
+        return nodeValue;
+    }
+
+    public double getOutputWeightAt(int index) {
+        return outputWeights.get(index);
+    }
 }
--- MarioLevelComp2011_cs4731/MarioLevelComp2011_cs4731/src/dk/itu/mario/ann/NeuralNetwork.java
+++ MarioLevelComp2011_cs4731/MarioLevelComp2011_cs4731/src/dk/itu/mario/ann/NeuralNetwork.java
@@ -3,9 +3,9 @@ package ann;
 import java.util.List;
 
 public interface NeuralNetwork {
-	
-	public List<Double> classifyInstance(Datum attribuesDatum);
-	
-	public double networkError(List<Datum> testingData);
-	
+
+    public List<Double> classifyInstance(Datum attribuesDatum);
+
+    public double networkError(List<Datum> testingData);
+
 }
